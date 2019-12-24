b6b5995fa159ec62e999c6a784519dc919c0d497
Susant <gracysusant@gmail.com>
2019-12-25 01:12:34 +0545

    To Do list UI

---
+++ src/components/Movies.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/Like";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleLiked = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+  render() {
+    const { length: moviesCount } = this.state.movies;
+    if (moviesCount === 0) return <p>There are no movies in the database.</p>;
+
+    return (
+      <div className="container">
+        <p>Showing {moviesCount} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClicked={() => this.handleLiked(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
